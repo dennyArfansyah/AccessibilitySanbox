2a41a49309fbbcb5143a256d77184c6715725056
dennyArfansyah <dennyarfansyah@gmail.com>
2023-02-08 16:09:19 +0700

    Initial Commit

---
+++ AccessibilitySanbox/AccessibilitySanboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySanboxApp.swift
+//  AccessibilitySanbox
+//
+//  Created by Denny Arfansyah on 08/02/23.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySanboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
